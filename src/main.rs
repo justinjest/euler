e87a809443d020583a94568745174b6bce7b204e
Jessica Cayaban <jest.cayaban@gmail.com>
2025-07-02 08:46:13 +0900

    Built in some simple bounds checking and found correct answer

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@
 mod utils;
 mod special_pythagorean_tripple;
 fn main() {
-    let (a, b, c) = special_pythagorean_tripple::find_val(12, 2);
-    println!("{:?}", (a, b, c));
+    let (a, b, c) = special_pythagorean_tripple::find_val(1000, 2);
+    println!("{:?}", (a*b*c));
 }
--- src/special_pythagorean_tripple.rs
+++ src/special_pythagorean_tripple.rs
@@ -13,14 +13,31 @@ pub fn find_val(val: usize,m: usize) -> (usize,usize,usize) {
     }
     for t in 1..m {
         let (a,b,c) = create_tripple(m, t);
-        println!("{:?}", (a, b, c));
         let sum = a + b + c;
+        println!("{sum}");
         if sum == val {
             return (a, b, c);
-        }else if sum > val {
-            println!("sum overwent val {sum}");
+        }
+// There needs to be some offramp, and I'm assuming by the time you get to val^2
+// You either will have found a valid tripple, or none exist
+// This may be an invalid assumption
+        else if sum > val*val {
+            println!("{m}, {t}");
+            println!("{:?}", (a, b, c));
             return (0, 0, 0);
         }
     }
     find_val(val, m+1)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    fn test_find_val() {
+        let res = find_val(12, 2);
+        assert_eq!(res, (3, 4, 5));
+        let res = find_val(234, 2);
+        assert_eq!(res, (65, 72, 97));
+    }
 }
